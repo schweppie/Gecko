c7224a619212895354f32010ff2f056ef39cacbb
Kamos85 <st_stijn15@hotmail.com>
2019-12-29 17:03:28 +0100

    ADD first robot script

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Utility;
 
-public class Field : MonoBehaviour
+public class Field : SingletonBehaviour<Field>
 {
     [SerializeField]
     private int fieldSize = 5;
     
     [SerializeField]
     private GameObject baseTile;
+
+    [SerializeField]
+    private Robot robot;
     
     private List<List<GameObject>> tiles = new List<List<GameObject>>();
     
-    void Start()
+    void Awake()
     {
         transform.position = new Vector3(-fieldSize/2.0f,0,-fieldSize/2.0f);
         for (int y = 0; y < fieldSize; y++)
@@ -26,6 +29,26 @@ public class Field : MonoBehaviour
                 tileRow.Add(tile);
             }
         }
+
+        robot.transform.SetParent(transform);
+        Vector2Int robotPos = new Vector2Int(
+            Mathf.Clamp(Mathf.RoundToInt(robot.transform.localPosition.x), 0, fieldSize-1),
+            Mathf.Clamp(Mathf.RoundToInt(robot.transform.localPosition.z), 0, fieldSize-1));
+        robot.transform.localPosition = new Vector3(robotPos.x, robot.transform.localPosition.y, robotPos.y);
+    }
+
+    public bool IsPositionOnGrid(Vector2Int position)
+    {
+        return GetTileOnPosition(position) != null;
+    }
+
+    public GameObject GetTileOnPosition(Vector2Int position)
+    {
+        if (position.x < 0 || position.x >= fieldSize)
+            return null;
+        if (position.y < 0 || position.y >= fieldSize)
+            return null;
+        return tiles[position.y][position.x];
     }
 
     void Update()
