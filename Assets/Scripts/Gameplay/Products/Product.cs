d2bb07625711e9bb3f988040054380713b2a133d
Kamos85 <st_stijn15@hotmail.com>
2020-01-20 22:34:19 +0100

    Add basic products

---
+++ Assets/Scripts/Gameplay/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Products
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Product")]
+    public class Product : ScriptableObject
+    {
+        [SerializeField]
+        private Color color;
+    }
+}
