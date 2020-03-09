29fb5182dd24df23f1a2558debb60f2e73de7c98
schweppie <joeppetersmail@gmail.com>
2020-03-09 18:23:31 +0100

    Add small cleanup

--- Assets/Scripts/Gameplay/Stations/Components/GarbageDispenserComponent.cs
+++ Assets/Scripts/Gameplay/Stations/Components/GarbageDispenserComponent.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Gameplay.Products;
+﻿using Gameplay.Products;
 using Gameplay.Tiles.Components;
 using UnityEngine;
 
@@ -22,24 +21,9 @@ namespace Gameplay.Stations.Components
             loadTileComponent.SetOutputProduct(mixedProductData);
         }
 
-        Product IProductProducer.ProduceProduct(ProductData productData)
+        public Product ProduceProduct(ProductData productData)
         {
-            // TODO: ProductsController should be responsible for instantiating
-            // product objects and product visual objects, should not be in station
-            MixedProduct mixedProduct = ProductsController.Instance.CreateProduct(productData, productSpawnPoint) as MixedProduct;
-/*
-            GameObject newProductVisualGO = Instantiate(mixedProductVisual.gameObject);
-            var newProductVisual = newProductVisualGO.GetComponent<ProductVisual>();
-            mixedProduct.SetVisual(newProductVisual);
-            var newMixedProductVisual = newProductVisualGO.GetComponent<MixedProductVisual>();
-            newMixedProductVisual.SetupMixedProduct();
-            newProductVisualGO.transform.position = productSpawnPoint.position;
-            newProductVisualGO.transform.rotation = productSpawnPoint.rotation;
-            
-            */
-            
-            
-            return mixedProduct;
+            return ProductsController.Instance.CreateProduct(productData, productSpawnPoint);
         }
 
         public bool CanProduceProduct(ProductData productData)
