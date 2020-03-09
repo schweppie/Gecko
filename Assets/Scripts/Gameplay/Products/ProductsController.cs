dec4bf842e1369518e480fdebb15bf6a0e80c61e
schweppie <joeppetersmail@gmail.com>
2020-03-09 17:26:05 +0100

    Add some cleanup

--- Assets/Scripts/Gameplay/Products/ProductsController.cs
+++ Assets/Scripts/Gameplay/Products/ProductsController.cs
@@ -1,5 +1,4 @@
 ﻿using JP.Framework.Flow;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Products
@@ -8,30 +7,17 @@ namespace Gameplay.Products
     {
         public Product CreateProduct(ProductData productData, Transform positionTransform)
         {
-            return CreateProduct(new List<ProductData>() { productData }, positionTransform);
-        }
-
-        public Product CreateProduct(List<ProductData> productDatas, Transform positionTransform)
-        {
             Product product;
 
-            if (productDatas.Count > 1)
-                product = new MixedProduct(productDatas);
+            if (productData.GetType() == typeof(MixedProductData))
+            {
+                MixedProductData mixedProductData = productData as MixedProductData;
+                product = new MixedProduct(mixedProductData.ProductDatas);
+            }
             else
             {
-                ProductData productData = productDatas[0];
-
-                if (productData.GetType() == typeof(MixedProductData))
-                {
-                    MixedProductData mixedProductData = productData as MixedProductData;
-                    product = new MixedProduct(mixedProductData.ProductDatas);
-                }
-                else
-                {
-                    product = new SingleProduct(productDatas[0]);                    
-                }
+                product = new SingleProduct(productData);                    
             }
-  
 
             return product;
         }
